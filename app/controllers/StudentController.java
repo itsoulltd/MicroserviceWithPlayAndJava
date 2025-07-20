472b85b8ca8d6c25d6d66d61be762a0855b7704e
Towhid Islam <m.towhid.islam@gmail.com>
2025-07-20 19:49:19 +0200

    feat: adding scafold for insert, update and delete api

--- app/controllers/StudentController.java
+++ app/controllers/StudentController.java
@@ -1,14 +1,17 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import domain.entities.Student;
 import domain.repositories.StudentRepository;
 import play.libs.Json;
+import play.mvc.Http;
 import play.mvc.Result;
 import utility.ResponseEntity;
 
 import javax.inject.Inject;
+import java.util.List;
 import java.util.Optional;
 
 import static play.mvc.Results.notFound;
@@ -35,5 +38,24 @@ public class StudentController {
             JsonNode jsonObjects = Json.toJson(student);
             return ok(ResponseEntity.ok(jsonObjects));
         }).orElse(notFound(ResponseEntity.notFound("Student with id:" + id + " not found")));
+    }
+
+    public Result retrieveAll() {
+        List<Student> result = repository.findAll(1, 10);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonData = mapper.convertValue(result, JsonNode.class);
+        return ok(ResponseEntity.ok(jsonData));
+    }
+
+    public Result create(Http.Request request) {
+        return ok(ResponseEntity.ok("UnderConstruction"));
+    }
+
+    public Result update(Http.Request request) {
+        return ok(ResponseEntity.ok("UnderConstruction"));
+    }
+
+    public Result delete(int id) {
+        return ok(ResponseEntity.ok("UnderConstruction"));
     }
 }
