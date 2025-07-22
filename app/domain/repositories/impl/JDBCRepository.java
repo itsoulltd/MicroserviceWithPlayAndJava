410939f75a75e71a2790ca99ed39afc7471723de
Towhid Islam <m.towhid.islam@gmail.com>
2025-07-22 17:21:45 +0200

    fix: JDBCRepository continue...

--- app/domain/repositories/impl/JDBCRepository.java
+++ app/domain/repositories/impl/JDBCRepository.java
@@ -1,7 +1,10 @@
 package domain.repositories.impl;
 
 import com.it.soul.lab.data.base.DataSource;
+import com.it.soul.lab.sql.SQLExecutor;
 import domain.repositories.Repository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import play.db.Database;
 
 import java.util.List;
@@ -9,42 +12,74 @@ import java.util.Optional;
 
 public abstract class JDBCRepository<ID, Entity> implements Repository<ID, Entity> {
 
-    @Override
-    public DataSource<ID, Entity> getDatasource() {
-        return null;
-    }
+    private static Logger LOG = LoggerFactory.getLogger(JDBCRepository.class);
 
     protected abstract Database getDb();
     public abstract Class<Entity> getEntityType();
     public abstract String getPrimaryKeyName();
 
     @Override
+    public DataSource<ID, Entity> getDatasource() {
+        return null;
+    }
+
+    @Override
     public long count() {
+        try (SQLExecutor executor = new SQLExecutor(getDb().getConnection())) {
+            //TODO:
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
         return 0;
     }
 
     @Override
     public Optional<Entity> findById(ID id) {
+        try (SQLExecutor executor = new SQLExecutor(getDb().getConnection())) {
+             //TODO:
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
         return Optional.empty();
     }
 
     @Override
     public List<Entity> findAll(int page, int limit) {
+        try (SQLExecutor executor = new SQLExecutor(getDb().getConnection())) {
+            //TODO:
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
         return null;
     }
 
     @Override
     public Optional<Entity> save(Entity entity) {
+        try (SQLExecutor executor = new SQLExecutor(getDb().getConnection())) {
+            //TODO:
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
         return Optional.empty();
     }
 
     @Override
     public Optional<Entity> update(Entity entity) {
+        try (SQLExecutor executor = new SQLExecutor(getDb().getConnection())) {
+            //TODO:
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
         return Optional.empty();
     }
 
     @Override
     public boolean delete(ID id) {
+        try (SQLExecutor executor = new SQLExecutor(getDb().getConnection())) {
+            //TODO:
+        } catch (Exception e) {
+            LOG.error(e.getMessage(), e);
+        }
         return false;
     }
 }
